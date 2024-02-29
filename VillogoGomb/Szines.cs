b9192b5be692758207833f7216021adf49e78c49
User <User@DESKTOP-K9MLIPK>
2024-02-29 12:51:57 +0100

    Add project files.

---
+++ VillogoGomb/Szines.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class Szines : Button
+    {
+        public Szines()
+        {
+            MouseClick += Szines_MouseClick;
+
+        }
+
+        private void Szines_MouseClick(object? sender, MouseEventArgs e)
+        {
+            BackColor = Color.Bisque;
+        }
+    }
+}
---
+++ VillogoGomb/szamolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class Szamolo : Button
+    {
+        int szám = 1;
+        public Szamolo()
+        {
+            Text=szám.ToString();
+            MouseClick += Szamolo_MouseClick;
+        }
+
+        private void Szamolo_MouseClick(object? sender, MouseEventArgs e)
+        {
+            szám++;
+            Text=szám.ToString();  
+
+
+        }
+    }
+}
